65802346a91ddb9da3bdae485ff3e6204f04d900
Vladimir Matena <matena@d3s.mff.cuni.cz>
2014-12-12 00:39:03 +0100

    Change result output files

--- RoadTrainsSimulation/src/cz/cuni/mff/d3s/roadtrain/demo/MeasureData.java
+++ RoadTrainsSimulation/src/cz/cuni/mff/d3s/roadtrain/demo/MeasureData.java
@@ -16,23 +16,32 @@ public class MeasureData {
 	static final Random rRandom = new Random(seed);
 	
 	//static final int[] CRASH_SITES = {1, 2, 3, 5, 10, 15, 20};
-	static final int[] CRASH_SITES = {2, 3};
 	
-	static final int RUNS = 1;
+	
+	static class Config {
+		static final int[] CRASH_SITES = {20};
+		static final int POLICE_PER_CRASH = 1;
+		static final int AMBULANCE_PER_CRASH = 2;
+		static final int FIRE_PER_CRASH = 2;
+		static final int RUNS = 1;
+		
+		static final String ident = String.format("%d%d%d", POLICE_PER_CRASH, AMBULANCE_PER_CRASH, FIRE_PER_CRASH);
+	}
+	
 	
 	public static void main(String[] args) throws Exception {
-		for(int sites: CRASH_SITES) {
+		for(int sites: Config.CRASH_SITES) {
 			
-			final File gMessages = new File("output" + File.separator + "groupers-" + sites);
+			final File gMessages = new File(String.format("output%s%s%sgroupers-%d", File.separator, Config.ident, File.separator, sites));
 			gMessages.getParentFile().mkdirs();
 			BufferedWriter gWriter = new BufferedWriter(new FileWriter(gMessages));
 			
-			final File rMessages = new File("output" + File.separator + "random-" + sites);
+			final File rMessages = new File(String.format("output%s%s%srandom-%d", File.separator, Config.ident, File.separator, sites));
 			rMessages.getParentFile().mkdirs();
 			BufferedWriter rWriter = new BufferedWriter(new FileWriter(rMessages));
 			
 			
-			for(int i = 0; i < RUNS; ++i) {
+			for(int i = 0; i < Config.RUNS; ++i) {
 				final int currentI = i;
 				
 				MessageProbe gProbe = new MessageProbe();
@@ -52,21 +61,22 @@ public class MeasureData {
 		
 	}
 	
-	private static void runSimulationWithGroupers(int crashes, int series, MessageProbe messageProbe) throws AnnotationProcessorException, IOException {
+	private static void runSimulationWithGroupers(int crashes, int series, MessageProbe messageProbe)
+			throws AnnotationProcessorException, IOException {
 		System.out.println("Launching simulation WITH groupers and " + crashes + " crash sites #" + series);
 				
 		LauncherWithGroupers launcher = new LauncherWithGroupers(3, gRandom, messageProbe);
 
-		final String ident = String.format("groupers-Crash%d-%d", crashes, series);
+		final String ident = String.format("%s%sgroupers-Crash%d-%d", Config.ident, File.separator, crashes, series);
 
 		final String outDir = "output" + File.separator + ident;
 		VehicleMonitor vehicleMonitor = new VehicleMonitor(outDir);
 		
 		DemoDeployer demoDeployer = new EmergencyDemoDeployer(
 				crashes,
-				Settings.AMBULANCE_PER_CRASH,
-				Settings.FIRE_PER_CRASH,
-				Settings.POLICE_PER_CRASH,
+				Config.AMBULANCE_PER_CRASH,
+				Config.FIRE_PER_CRASH,
+				Config.POLICE_PER_CRASH,
 				(VehicleDeployer) launcher,
 				vehicleMonitor);
 		
@@ -78,16 +88,16 @@ public class MeasureData {
 				
 		LauncherWithRandomIPGossip launcher = new LauncherWithRandomIPGossip(rRandom, messageProbe);
 
-		final String ident = String.format("random-Crash%d-%d", crashes, series);
+		final String ident = String.format("%s%srandom-Crash%d-%d", Config.ident, File.separator, crashes, series);
 
 		final String outDir = "output" + File.separator + ident;
 		VehicleMonitor vehicleMonitor = new VehicleMonitor(outDir);
 		
 		DemoDeployer demoDeployer = new EmergencyDemoDeployer(
 				crashes,
-				Settings.AMBULANCE_PER_CRASH,
-				Settings.FIRE_PER_CRASH,
-				Settings.POLICE_PER_CRASH,
+				Config.AMBULANCE_PER_CRASH,
+				Config.FIRE_PER_CRASH,
+				Config.POLICE_PER_CRASH,
 				(VehicleDeployer) launcher,
 				vehicleMonitor);
 		
